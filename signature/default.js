603eea3f70912b276ab0861c50fd01a15718a9b1
SyncfusionBuild <buildautomation@syncfusion.com>
2022-04-04 15:27:22 +0530

    v20.1.0.47

--- signature/default.js
+++
@@ -1,4 +0,0 @@
-syncApp.controller('signatureCtrl', function ($scope) {
-	   $scope.height = 400,
-      $scope.strokewidth = 3
-});
--- uploadbox/default.js
+++
@@ -1,4 +0,0 @@
-syncApp.controller('UploadboxCtrl', function ($scope) {
-	$scope.saveurl = window.baseurl + "api/uploadbox/Save";
-	$scope.removeurl = window.baseurl + "api/uploadbox/Remove";
-       });
